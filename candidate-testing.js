d791f0d190e415a5f9a994c6a292fc79f8cb2c4c
vSutherland <142331739+vSutherland@users.noreply.github.com>
2023-09-18 15:20:32 -0500

    part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName; ("")
+let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question; ("Who was the first American woman in space?");
 let correctAnswer; ("Sally Ride");
@@ -17,8 +17,8 @@ let candidateAnswers;
 
 
 function askForName() {
-  const name = prompt("What's your name?");
-return name;
+  const candidateName = prompt("What's your name?");
+return candidateName;
 }
 
 function askQuestion(name) {
