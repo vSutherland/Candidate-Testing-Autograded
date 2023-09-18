fbdb6973f09de90e6f1b5962014872d30c4c4613
vSutherland <142331739+vSutherland@users.noreply.github.com>
2023-09-18 11:55:17 -0500

    Saved for later

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName; 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question; ("Who was the first American woman in space?");
+let correctAnswer; ("Sally Ride");
+let candidateAnswer; ""
 
 
 //TODO: Variables for Part 2
@@ -17,13 +17,20 @@ let candidateAnswers;
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+  const name = prompt("What's your name?");
+return name;
 }
 
-function askQuestion() {
+function askQuestion(name) {
+  const question = "Who was the first American woman in space?";
+  const answer = "Sally Ride";
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  const candidateAnswer = prompt(question);
+  if (candidateAnswer.toLowerCase() === answer.toLowerCase()) {
+    alert("Correct! Well done, " + name + "!");
+} else {
+    alert("Incorrect, " + name + ". The correct answer is Sally Ride.");
+}
 
 }
 
