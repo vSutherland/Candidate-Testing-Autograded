2a679f208f1fc81c477bbd9ba47d6f7650e6ba0c
vSutherland <142331739+vSutherland@users.noreply.github.com>
2023-09-19 11:50:13 -0500

    Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,54 +3,52 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer;
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = "True or false: 5 kilometer == 5000 meters?";
-let correctAnswers = "true";
+let questions;
+let correctAnswers;
 let candidateAnswers;
 
 
 function askForName() {
-  const candidateName = ("What's your name?");
-return candidateName;
+  const candidateName = input.question("What's your name?");
+  return candidateName;
 }
 
 function askQuestion(name) {
-  const question = "Who was the first American woman in space?";
-  const answer = "Sally Ride";
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  const candidateAnswer = prompt(question);
-  
-
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
-  if (candidateAnswer === correctAnswer) {
-    console.log("Correct! Well done!");
-} else {
-    console.log("Incorrect, The correct answer is Sally Ride.");
-}
+  let correctCount = 0;
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      console.log(`Question ${i + 1}: Correct!`);
+      correctCount++;
+    } else {
+      console.log(`Question ${i + 1}: Incorrect. The correct answer is ${correctAnswers[i]}.`);
+    }
+  }
 
-
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let grade = (correctCount / questions.length) * 100;
+  console.log(`Candidate Score: ${grade}%`);
 
   return grade;
 }
 
+
 function runProgram() {
   const candidateName = askForName();
-  alert("Hello, " + candidateName + "! Let's take a quiz.");
+  console.log("Hello, " + candidateName + "! Let's take a quiz.");
   askQuestion(candidateName);
+  gradeQuiz(candidateAnswer);
 }
 
 
