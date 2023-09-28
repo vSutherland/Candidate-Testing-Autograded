ce878b77a5c0419446759810301224e36a37bf70
vSutherland <142331739+vSutherland@users.noreply.github.com>
2023-09-28 11:48:58 -0500

    Passing Test

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,29 +1,38 @@
- // Part one done //
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
 let candidateName = "";
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
+// Variables for Part 2
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
 
-//TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
 
+let candidateAnswers = [];
 
 function askForName() {
-  const candidateName = input.question("What's your name?");
+  candidateName = input.question("What's your name? ");
   return candidateName;
 }
 
-function askQuestion(name) {
-  candidateAnswer = input.question(question);
+function askQuestion() {
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -44,16 +53,13 @@ function gradeQuiz(candidateAnswers) {
   return grade;
 }
 
-
 function runProgram() {
-  const candidateName = askForName();
+  askForName();
   console.log("Hello, " + candidateName + "! Let's take a quiz.");
-  askQuestion(candidateName);
-  gradeQuiz(candidateAnswer);
+  askQuestion();
+  gradeQuiz(candidateAnswers);
 }
 
-
-// ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
   question: question,
